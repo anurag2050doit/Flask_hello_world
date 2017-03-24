0486bd042122e695d2e30cbeb4dc69541c170440
Anurag <anurag2050@gmail.com>
2017-03-24 15:33:27 +0530

    Redirect added

--- hello.py
+++ hello.py
@@ -1,15 +1,29 @@
-from flask import Flask, url_for
+from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == "POST":
+        if valid_login(
+            request.form.get('username'),
+            request.form.get('password')
+        ):
+            return redirect(url_for('welcome',username=request.form.get('username')))
+        else:
+            error = "Incorrect Username and password"
+    return render_template('login.html', error=error)
 
+@app.route('/welcome/<username>')
+def welcome(username):
+    return render_template('welcome.html',username=username)
 
-@app.route('/profile/<username>')
-def show_user_profile(username):
-    return "User: %s" % username
-
-@app.route('/')
-def index():
-    return url_for('show_user_profile', username='anurag')
+def valid_login(username, password):
+    # checks on the db if the username and password are correct
+    if len(password)>5 and username:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     app.debug = True
